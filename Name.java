7fd66f865ff0562af1930edaf92f36b2aa75e8c0
james-schuebel <99284697+james-schuebel@users.noreply.github.com>
2022-10-31 17:53:56 -0500

    Update Name.java

--- Name.java
+++ Name.java
@@ -1,4 +1,4 @@
-class Name extends BaseToken implements Checkable {
+class Name extends BaseToken {
   String id;
   Expr index;
   public Name(String id) {
@@ -15,23 +15,5 @@ class Name extends BaseToken implements Checkable {
 
   private boolean hasIndex() {
     return index != null;
-  }
-
-  public boolean isArray() throws Exception {
-    return table.getVar(id).isArray() && !hasIndex();
-  }
-  public boolean isFinal() throws Exception {
-    return table.getVar(id).isFinal();
-  }
-  public boolean isFunction() throws Exception {
-    return table.getVar(id).isFunction();
-  }
-  
-  
-  public SymTable.Type typeCheck() throws Exception {
-    if (!hasIndex() || index.typeCheck().equals(SymTable.Type.INT))
-      return table.getVar(id).typeCheck();
-    else 
-      throw new Exception("array index must be an INT!");
   }
 }
